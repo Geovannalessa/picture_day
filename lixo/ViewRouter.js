1715df5963017eda8348b6c9e1a72146a3cca9d0
Geovanna Lessa <geovannalessa2@gmail.com>
2022-12-05 11:01:04 -0300

    Arquivos que envolvem express, excluidos

--- lixo/ViewRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const ViewController = require("../controllers/ViewController");
-const router = express.Router();
-
-router.get('/', ViewController.monstraHome);
-router.get('/history', ViewController.monstraHistory);
-
-module.exports = router;
